ccb80ce151ac862c7580e9abd77ae9f9e462e7eb
Kologo Boinzemwinde Josias Yannick <bjykologo@SQLI-BJYKOLOGO.SQLI>
2019-02-21 17:24:13 +0100

    Fisrt commit

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { Switch, Route } from 'react-router-dom';
+import Home from './Home'
+import Schedule from './Schedule';
+import Roster from './Roster';
+
+const Main = () => (
+    <main>
+        <Switch>
+            <Route exact path='/' componenet={Home} />
+            <Route path='/roster' component={Roster} />
+            <Route path='schedule' component={Schedule}/>
+        </Switch>
+    </main>
+)
+
+export default Main;
---
+++ src/components/Roster.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import FullRoster from './FullRoster';
+import Player from './Player';
+
+const Roster = () => (
+    <Switch>
+        <Route exact path='/Roster' component={FullRoster} />
+        <Route path = '/Roster/:number' component={Player} />
+    </Switch>
+)
+
+export default Roster;
