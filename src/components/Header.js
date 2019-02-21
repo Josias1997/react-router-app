3e947780db4eb20d5cbd893566a513d05349bef2
Kologo Boinzemwinde Josias Yannick <bjykologo@SQLI-BJYKOLOGO.SQLI>
2019-02-21 18:31:44 +0100

    Boostrap setting unfinished

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () =>(
-    <header>
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/roster">Roster</Link></li>
-                <li><Link to="/schedule">Schedule</Link></li>
-            </ul>
-        </nav>
+    <header className="masthead mb-auto">
+        <div className="inner">
+            <h3 className="masthead-brand">Sport Team App</h3>
+            <nav className="nav nav-masthead justify-content-center">
+                <ul>
+                    <li className="nav nav-link active"><Link to="/">Home</Link></li>
+                    <li className="nav nav-link"><Link to="/roster">Roster</Link></li>
+                    <li className="nav nav-link"><Link to="/schedule">Schedule</Link></li>
+                </ul>
+            </nav>
+        </div>
     </header>
 )
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
     <BrowserRouter>
